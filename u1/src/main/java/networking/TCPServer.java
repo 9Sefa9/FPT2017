b29298a2e13dc15a6622971db56247b7e85e2d2d
Sefa Laptop <sefagoevercin@hotmail.de>
2017-07-04 17:36:51 +0200

    first attempt

--- u1/src/main/java/networking/TCPServer.java
+++ u1/src/main/java/networking/TCPServer.java
@@ -1,26 +1,76 @@
 package networking;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.server.UnicastRemoteObject;
 
-public class TCPServer extends UnicastRemoteObject implements IContainer{
+import java.io.*;
+import java.net.*;
 
-    public TCPServer() throws RemoteException{
+public class TCPServer extends Thread {
+
+    protected String password;
+    //nimmt Client entgegen.
+    public void run() {
+        try (ServerSocket server = new ServerSocket(5020)) {
+            int connections = 0;
+
+            while (true) {
+                try {
+                    Socket socket = server.accept();
+                    connections++;
+                    new TCPServerThread(connections, socket).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+}
+  //Stellt eine Verbindung mit dem Client auf.
+  class TCPServerThread extends Thread {
+    private int name;
+    private Socket socket;
+
+    public TCPServerThread(int name, Socket socket) {
+        this.name = name;
+        this.socket = socket;
+    }
+
+    public void run() {
+        System.out.println("Connected to Client #:"+name+"...");
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             OutputStream out = socket.getOutputStream()) {
+
+            while(true){
+            if((in.readLine().equals(this.password));
+
+            }
+            out.write(2);
+            out.flush();
+
+            System.out.println("Connection #"+ name +" closed");
+            socket.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+    }
+}
+
     /*
-    public void initialization() throws RemoteException,MalformedURLException{
-        LocateRegistry.createRegistry(5020);
-
-        //Remote tcpcontainer = new IContainer();
-        //Naming.rebind("//localhost:1099/tcpcontainer", tcpcontainer);
-        // System.out.println("tcpcontainer started...");
+    public TCPServer() throws RemoteException,MalformedURLException {
+        super();
+        initialization();
     }
-    */
+
+    public void initialization() throws RemoteException,MalformedURLException{
+        TCPServer tcp = new TCPServer();
+        Registry registry = LocateRegistry.createRegistry(5020);
+        registry.rebind("//localhost:1099/tcp", tcp);
+
+    }
+
     @Override
     public void setPassword(String pw) throws RemoteException {
 
@@ -45,7 +95,8 @@ public class TCPServer extends UnicastRemoteObject implements IContainer{
     public String getName() throws RemoteException {
         return null;
     }
-}
+    */
+
 
 
 
